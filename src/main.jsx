f89277d45f45c5c334ecfcc44a2345c05b47ada6
erma swartz` <ermaswartz@ermas-MacBook-Pro.local>
2024-09-03 13:49:28 -0400

    fixing Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Authenticator } from "@aws-amplify/ui-react";
 import App from './App';
 import NextPage from './NextPage'; // Import the new page component
 
 function Main() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route path="/next" element={<NextPage />} />
-      </Routes>
-    </Router>
+    <Authenticator>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route path="/next" element={<NextPage />} />
+        </Routes>
+      </Router>
+    </Authenticator>
   );
 }
 
